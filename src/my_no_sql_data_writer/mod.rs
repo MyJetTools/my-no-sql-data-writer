50f9666d02bd31c2be15f2a781ecf2b49b788ff9
amigin <amigin@gmail.com>
2023-02-24 22:52:54 +0200

    Fixes

--- src/my_no_sql_data_writer/mod.rs
+++ src/my_no_sql_data_writer/mod.rs
@@ -2,5 +2,5 @@ mod error;
 mod my_no_sql_data_writer;
 mod update_read_statistics;
 pub use error::DataWriterError;
-pub use my_no_sql_data_writer::MyNoSqlDataWriter;
+pub use my_no_sql_data_writer::*;
 pub use update_read_statistics::*;
